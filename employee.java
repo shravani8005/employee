735abefc5635a607b5a7f079b764ca819ae6358d
shravani8005/ <shravani8005@gmail.com>
2025-05-07 02:10:25 -0700

    save

--- employee.java
+++ employee.java
@@ -2,19 +2,22 @@ public class employee{
   String name;
   int id;
   double salary;
-  public employee(String name,int id,double salary){
+  String addr;
+  public employee(String name,int id,double salary,String addr){
     this.name=name;
     this.id=id;
     this.salary=salary;
+    this.addr=addr;
   }
   public void displayInfo(){
     System.out.println("employee id:" + id);
     System.out.println("employee name:" + name);
     System.out.println("salary:" + salary);
+    System.out.println("addresss: "+addr);
 
   }
   public static void main(String[] args) {
-    employee e=new employee("abc",101,50000);
+    employee e=new employee("abc",101,50000,"Dharwad");
     e.displayInfo();
   }
 }
